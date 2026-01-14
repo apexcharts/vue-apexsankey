7f2d58f3bf14f6a5f1748fc91b2aab571d8295f0
Juned Chhipa <juned.chhipa@gmail.com>
2026-01-14 23:32:02 +0530

    workflow for deploying the demo app to GH pages

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
 
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/vue-apexsankey/" : "/",
   plugins: [vue()],
   resolve: {
     alias: {
-      'vue-apexsankey': resolve(__dirname, '../src/index.ts')
-    }
-  }
-})
+      "vue-apexsankey": resolve(__dirname, "../src/index.ts"),
+    },
+  },
+});
